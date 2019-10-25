cb07e52945b723f94d83c2da90d6ad61c9052cbb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-25 05:21:09 +0000

    Deploy julianrojas87/Tutorial-ISWC2019-LDflex-on-React to github.com/julianrojas87/Tutorial-ISWC2019-LDflex-on-React.git:gh-pages

---
+++ precache-manifest.145435a37814586805aa89240177aa87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1d765721d82c302161cc62f547e0de",
+    "url": "/Tutorial-ISWC2019-LDflex-on-React/index.html"
+  },
+  {
+    "revision": "f5d2d3e58446c53c0d69",
+    "url": "/Tutorial-ISWC2019-LDflex-on-React/static/css/main.2054a78b.chunk.css"
+  },
+  {
+    "revision": "9ec894603a4ba6a74bbf",
+    "url": "/Tutorial-ISWC2019-LDflex-on-React/static/js/2.c5e0b902.chunk.js"
+  },
+  {
+    "revision": "f5d2d3e58446c53c0d69",
+    "url": "/Tutorial-ISWC2019-LDflex-on-React/static/js/main.2a397acf.chunk.js"
+  },
+  {
+    "revision": "c9ff0347bfa826bd83b7",
+    "url": "/Tutorial-ISWC2019-LDflex-on-React/static/js/runtime-main.3ceab48b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f27902a5e8c1da3acccca6ab97d2b61.js"
+  "/Tutorial-ISWC2019-LDflex-on-React/precache-manifest.145435a37814586805aa89240177aa87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tutorial-ISWC2019-LDflex-on-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
